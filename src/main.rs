19e61741da6d3c1d199e9300bba5e8bc3bd72c48
Chidiogo <chidiogoezeh.me@gmail.com>
2026-08-25 03:00:22 +0100

    Update: created tuple struct and menu enum

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ struct Expence {
     category: String,
 }
 
+struct Expences(u32);
+
+enum MenuOption {
+    Add,
+    Delete,
+    Total,
+    List,
+    Exit,
+}
+
 fn main() {
     println!("Let's work on wekk 3 project!");
 }
