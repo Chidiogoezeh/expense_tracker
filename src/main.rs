5fe8617f266b9fe1da74680c2310b9b4c2f1c3e2
Chidiogo <chidiogoezeh.me@gmail.com>
2026-08-25 08:55:40 +0100

    Update: creating custom error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-struct Expence {
-    id: ExpenceId,
+struct Expense {
+    id: ExpenseId,
     description: String,
     amount: f64,
     category: String,
 }
 
-struct Expences(u32);
+struct ExpenseId(u32);
 
 enum MenuOption {
     Add,
@@ -15,6 +15,35 @@ enum MenuOption {
     Exit,
 }
 
+#[derive(Debug)]
+enum ExpenseError {
+    ExpenseNotFound,
+    InvalidAmount,
+}
+
+// Simple example
+fn add_expense(amount: f64) -> Result<(), ExpenseError> {
+    if amount <= 0.0 {
+        return Err(ExpenseError::InvalidAmount);
+    }
+
+    println!("Expense is valid");
+
+    Ok(())
+}
+
 fn main() {
-    println!("Let's work on wekk 3 project!");
+    let result = add_expense(5000.0);
+
+    match result {
+        Ok(()) => println!("Expense added successfully."),
+
+        Err(ExpenseError::InvalidAmount) => {
+            println!("Amount must be greater than zero.");
+        }
+
+        Err(ExpenseError::ExpenseNotFound) => {
+            println!("Expense was not found")
+        }
+    }
 }
